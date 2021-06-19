ed31da1bc62e971fd9571110d8266e90f346caa5
Ittipol Jannapast <westkitseo@gmail.com>
2021-06-19 10:21:18 +0700

    init demo1

---
+++ demo1/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{backgroundColor: '#FF0'}}>
+      {/* JSX */}
+      <Text>Anacondong</Text>
+    </View>
+  );
+}
