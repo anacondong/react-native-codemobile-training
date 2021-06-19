9041f573d05317097782514b58fa5e82207313e2
Ittipol Jannapast <westkitseo@gmail.com>
2021-06-19 11:09:06 +0700

    done demo1

--- demo1/App.js
+++ demo1/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-    <View style={{backgroundColor: '#FF0'}}>
+    <View
+      style={{
+        backgroundColor: '#000',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
       {/* JSX */}
-      <Text>Anacondong</Text>
+      <Text style={{...styles.item, backgroundColor: '#0FF'}}>box1</Text>
+      <Text style={{...styles.item, backgroundColor: '#FFF'}}>box2</Text>
+      <Text style={{...styles.item, backgroundColor: 'red'}}>box3</Text>
+      <Text style={{...styles.item, backgroundColor: 'blue'}}>box4</Text>
+      <Text style={{...styles.item, backgroundColor: '#DDD'}}>box5</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  item: {
+    fontSize: 30,
+    // flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+});
